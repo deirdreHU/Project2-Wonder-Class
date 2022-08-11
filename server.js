3c5458e7a33498f60bd5af3e247f659b9207a5fe
Deirdre Hu <deirdre.hu@edoovo.com>
2022-08-11 22:34:00 +0800

    change wordings

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const session = require('express-session')
 
-const UsersController = require("./controllers/users/users_controller");
+// const UsersController = require("./controllers/users/users_controller");
 const PagesController = require("./controllers/pages/pages_controllers");
-const ClassesController = require("./controllers/classes/classes_controllers");
-const StoriesController = require("./controllers/Stories/stories_controller");
-const {registerValidator, loginValidator, createClassValidator,resetPasswordValidator} = require("./middlewares/validator");
-const {isAuthenticated, isTeacher} = require("./middlewares/auth.middleware");
+// const ClassesController = require("./controllers/classes/classes_controllers");
+// const StoriesController = require("./controllers/stories/stories_controller");
+// const {registerValidator, loginValidator, createClassValidator,resetPasswordValidator} = require("./middlewares/validator");
+// const {isAuthenticated, isTeacher} = require("./middlewares/auth.middleware");
 
 // Set view engine
 app.set('view engine', 'ejs')
@@ -31,52 +31,52 @@ app.use(session({
 
 app.get('/', PagesController.showEntry);
 
-app.get('/home', isAuthenticated, PagesController.showHome);
+// app.get('/home', isAuthenticated, PagesController.showHome);
 
-app.get('/users/register', PagesController.showRegister);
-app.post('/users/register', registerValidator, UsersController.register);
+// app.get('/users/register', PagesController.showRegister);
+// app.post('/users/register', registerValidator, UsersController.register);
 
-app.get('/users/resetpassword', PagesController.showResetPassword);
-app.post('/users/resetpassword', resetPasswordValidator, UsersController.resetPassword);
+// app.get('/users/resetpassword', PagesController.showResetPassword);
+// app.post('/users/resetpassword', resetPasswordValidator, UsersController.resetPassword);
 
-app.get('/users/login/student', PagesController.showStudentLogin);
-app.get('/users/login/teacher', PagesController.showTeacherLogin);
-app.post('/users/login', loginValidator, UsersController.login);
+// app.get('/users/login/student', PagesController.showStudentLogin);
+// app.get('/users/login/teacher', PagesController.showTeacherLogin);
+// app.post('/users/login', loginValidator, UsersController.login);
 
-app.get('/users/logout', UsersController.logout);
-app.get('/users/profile', isAuthenticated, PagesController.showProfile);
+// app.get('/users/logout', UsersController.logout);
+// app.get('/users/profile', isAuthenticated, PagesController.showProfile);
 
-app.get('/students/find/:student_name', isAuthenticated, isTeacher, UsersController.findStudentNames); 
+// app.get('/students/find/:student_name', isAuthenticated, isTeacher, UsersController.findStudentNames); 
 
-app.post('/class/create', isAuthenticated, isTeacher, createClassValidator, ClassesController.createClass);
-app.get('/class/create', PagesController.showCreateClass);
-app.get('/class/delete/:classID', isAuthenticated, isTeacher, ClassesController.deleteClass);
+// app.post('/class/create', isAuthenticated, isTeacher, createClassValidator, ClassesController.createClass);
+// app.get('/class/create', PagesController.showCreateClass);
+// app.get('/class/delete/:classID', isAuthenticated, isTeacher, ClassesController.deleteClass);
 
-app.get('/class/:classID', isAuthenticated, PagesController.showClass);
+// app.get('/class/:classID', isAuthenticated, PagesController.showClass);
 
-app.get('/class/:classID/students', isAuthenticated, isTeacher, PagesController.showClassStudents);
-app.get('/class/:classID/stories', isAuthenticated, isTeacher, PagesController.showClassStories);
-app.get('/class/detail/:classID', isAuthenticated, isTeacher, (req, res) => {
-    const {classID} = req.params;
-    res.redirect(`/class/${classID}/students`);
-});
+// app.get('/class/:classID/students', isAuthenticated, isTeacher, PagesController.showClassStudents);
+// app.get('/class/:classID/stories', isAuthenticated, isTeacher, PagesController.showClassStories);
+// app.get('/class/detail/:classID', isAuthenticated, isTeacher, (req, res) => {
+//     const {classID} = req.params;
+//     res.redirect(`/class/${classID}/students`);
+// });
 
-app.get('/class/:classID/students/add', isAuthenticated, isTeacher, PagesController.showAddStudent);
-app.post('/class/:classID/students/add', isAuthenticated, isTeacher, ClassesController.addStudent);
-app.get('/class/:classID/students/:studentID/delete', isAuthenticated, isTeacher, ClassesController.deleteStudent);
+// app.get('/class/:classID/students/add', isAuthenticated, isTeacher, PagesController.showAddStudent);
+// app.post('/class/:classID/students/add', isAuthenticated, isTeacher, ClassesController.addStudent);
+// app.get('/class/:classID/students/:studentID/delete', isAuthenticated, isTeacher, ClassesController.deleteStudent);
 
-app.get('/class/:classID/stories/add', isAuthenticated, isTeacher, PagesController.showAddStory);
-app.post('/class/:classID/stories/add', isAuthenticated, isTeacher, ClassesController.addStory);
-app.get('/class/:classID/stories/:storyID/delete', isAuthenticated, isTeacher, ClassesController.deleteStory);
+// app.get('/class/:classID/stories/add', isAuthenticated, isTeacher, PagesController.showAddStory);
+// app.post('/class/:classID/stories/add', isAuthenticated, isTeacher, ClassesController.addStory);
+// app.get('/class/:classID/stories/:storyID/delete', isAuthenticated, isTeacher, ClassesController.deleteStory);
 
-app.get('/story/:storyID', isAuthenticated, PagesController.showStory);
-app.post('/story/:storyID/comment', isAuthenticated, StoriesController.addStorycomment);
+// app.get('/story/:storyID', isAuthenticated, PagesController.showStory);
+// app.post('/story/:storyID/comment', isAuthenticated, StoriesController.addStorycomment);
 
-app.get('/profile/teacher', isAuthenticated, isTeacher, PagesController.showTeacherProfile);
-app.post('/profile/teacher', isAuthenticated, isTeacher, UsersController.updateTeacherProfile);
+// app.get('/profile/teacher', isAuthenticated, isTeacher, PagesController.showTeacherProfile);
+// app.post('/profile/teacher', isAuthenticated, isTeacher, UsersController.updateTeacherProfile);
 
-app.get('/profile/student', isAuthenticated, PagesController.showStudentProfile);
-app.post('/profile/student', isAuthenticated, UsersController.updateStudentProfile);
+// app.get('/profile/student', isAuthenticated, PagesController.showStudentProfile);
+// app.post('/profile/student', isAuthenticated, UsersController.updateStudentProfile);
 
 app.listen(PORT, async () => {
     try {
